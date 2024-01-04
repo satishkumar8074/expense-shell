504322f844a5c5a3637d2047a5130cd69c4c3171
Vankadarisatishkumar <vankadarisatishkumar8074@gmail.com>
2024-01-05 01:13:17 +0530

    automation bacckend and mysql expense

--- backend.sh
+++ backend.sh
@@ -6,7 +6,6 @@ dnf install nodejs -y
 cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
-
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
--- mysql.sh
+++ mysql.sh
@@ -3,7 +3,6 @@ dnf module disable mysql -y
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
 dnf install mysql-community-server -y
-
 systemctl enable mysqld
 systemctl start mysqld
 
