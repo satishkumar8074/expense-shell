d5aca54e7fd3693384fc6011c67bc2d28ae971d0
Vankadarisatishkumar <vankadarisatishkumar8074@gmail.com>
2024-01-05 23:11:32 +0530

    corrected the erros manually

--- backend.sh
+++ backend.sh
@@ -15,7 +15,7 @@ unzip /tmp/backend.zip
 cd /app
 npm install
 dnf install mysql -y
-mysql -h mysql.satishdevops97.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.172.31.20.174 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
--- mysql.sh
+++ mysql.sh
@@ -3,6 +3,7 @@ dnf module disable mysql -y
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 
 dnf install mysql-community-server -y
+
 systemctl enable mysqld
 systemctl start mysqld
 
